2cb95a05041a2de0922968e01d3f22a0bf041650
Gabriel Nunes <70154660+gabrielnm123@users.noreply.github.com>
2024-06-29 18:47:53 -0300

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -2,14 +2,9 @@
 
 apt update && apt install -y curl ssh
 clear
-echo "Chave SSH Git"
-echo
-echo -n "Email da sua conta GitHub: "
-read email
-ssh-keygen -t ed25519 -C "$email"
+ssh-keygen -t ed25519
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_ed25519
-echo
 echo "Copie o texto abaixo para adiciona-lá na plataforma: "
 echo
 cat ~/.ssh/id_ed25519.pub
